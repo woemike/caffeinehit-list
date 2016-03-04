6050c45a54925bf1fc9bb36e8a821395e131442b
Alexandru Mihai Ion <ionmihai09@gmail.com>
2016-03-04 00:04:42 +0000

    bugs
    bugs

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,25 @@
 var app = angular.module('caffeinehit', [
-	'ionic',
+  'ionic',
   'ngCordova',
-	'caffeinehit.controllers',
-	'caffeinehit.services',
-	'caffeinehit.filters'
+  'caffeinehit.controllers',
+  'caffeinehit.services',
+  'caffeinehit.filters'
 ]);
 
 app.run(function ($ionicPlatform) {
-	$ionicPlatform.ready(function () {
-		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		// for form inputs)
-		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-			cordova.plugins.Keyboard.disableScroll(true);
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-		}
-		if (window.StatusBar) {
-			// org.apache.cordova.statusbar required
-			StatusBar.styleDefault();
-		}
-	});
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
 });
 
 app.config(function ($httpProvider) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,32 +1,32 @@
 var app = angular.module('caffeinehit.services', []);
 
 app.service("YelpService", function ($q, $http, $cordovaGeolocation, $ionicPopup) {
-	var self = {
-		'page': 1,
-		'isLoading': false,
-		'hasMore': true,
-		'results': [],
-		'lat': 51.544440,
-		'lon': -0.022974,
-		'refresh': function () {
-			self.page = 1;
-			self.isLoading = false;
-			self.hasMore = true;
-			self.results = [];
-			return self.load();
-		},
-		'next': function () {
-			self.page += 1;
-			return self.load();
-		},
-		'load': function () {
-			self.isLoading = true;
-			var deferred = $q.defer();
+  var self = {
+    'page': 1,
+    'isLoading': false,
+    'hasMore': true,
+    'results': [],
+    'lat': 51.544440,
+    'lon': -0.022974,
+    'refresh': function () {
+      self.page = 1;
+      self.isLoading = false;
+      self.hasMore = true;
+      self.results = [];
+      return self.load();
+    },
+    'next': function () {
+      self.page += 1;
+      return self.load();
+    },
+    'load': function () {
+      self.isLoading = true;
+      var deferred = $q.defer();
 
-      ionic.Platform.ready(function (){
+      ionic.Platform.ready(function () {
         $cordovaGeolocation
-          .getCurrentPosition({timeout:5000, enableHighAccuracy:false})
-          .then(function (position){
+          .getCurrentPosition({timeout: 10000, enableHighAccuracy: false})
+          .then(function (position) {
             self.lat = position.coords.latitude;
             self.lon = position.coords.longitude;
 
@@ -36,9 +36,9 @@ app.service("YelpService", function ($q, $http, $cordovaGeolocation, $ionicPopup
               lon: self.lon
             };
 
+
             $http.get('https://codecraftpro.com/api/samples/v1/coffee/', {params: params})
               .success(function (data) {
-                self.isLoading = false;
                 console.log(data);
 
                 if (data.businesses.length == 0) {
@@ -49,27 +49,29 @@ app.service("YelpService", function ($q, $http, $cordovaGeolocation, $ionicPopup
                   });
                 }
 
+                self.isLoading = false;
                 deferred.resolve();
               })
               .error(function (data, status, headers, config) {
                 self.isLoading = false;
                 deferred.reject(data);
               });
-          }, function (err){
-              console.error("Error getting position");
-              console.error(err);
-              $ionicPopup.alert({
-                'title':'Please switch on geolocation',
-                'template':"It seems like you've switched off geolocation for caffeinehit"
-              })
-          });
+
+          }, function (err) {
+            console.error("Error getting position");
+            console.error(err);
+            $ionicPopup.alert({
+              'title':'Please switch on geolocation',
+              'template': "It seems like you've switched off geolocation for caffeinehit, please switch it on by going to you application settings."
+            });
+          })
       });
 
-			return deferred.promise;
-		}
-	};
+      return deferred.promise;
+    }
+  };
 
-	self.load();
+  self.load();
 
-	return self;
+  return self;
 });
